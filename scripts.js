63cd92e8b3c2ba0607aa62bd5c216867b0945339
unknown <emersonh16@gmail.com>
2022-09-08 11:40:39 -0400

    basic styles and adding event listeners

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const winCondition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+const Gameboard = (()=>{
+    let gameboard = ["","","","","","","","",""]
+
+
+    // reset array
+    //update array 
+    //test if game is complete
+})
+
+
+
+const Player = (name, symbol) => {
+    
+
+
+}
+
+function myfunction(){
+    document.getElementById("box1").innerText = "X"
+}
+
+
+document.getElementById("box1").addEventListener("click", myfunction)
+
+
+
+
+__REACT_DEVTOOLS_GLOBAL_HOOK__ = true;
