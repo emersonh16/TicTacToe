658fb744a2105131853af0ef44a20d05bcf9f6cf
unknown <emersonh16@gmail.com>
2022-09-23 12:40:48 -0400

    emerson's solution in progress

--- scripts.js
+++ scripts.js
@@ -1,39 +1,57 @@
-const winCondition = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6],
-]
+/*const gameboard = (() => {
+    let board = ["","","","","","","","",""]
 
-const Gameboard = (()=>{
-    let gameboard = ["","","","","","","","",""]
+    const setField = (index, gamesign) => {
+        board[index] = gamesign
+        console.log(board)
+    }
 
+    return {setField}
+});
+*/
 
-    // reset array
-    //update array 
-    //test if game is complete
-})
+const gameController = (() => {
+    let board = ["","","","","","","","",""]
+    let round = 1 
+    playerToken = "X"
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];  
+    const fieldElements = document.querySelectorAll(".field")
 
-
-
-const Player = (name, symbol) => {
+    fieldElements.forEach(clickfunction)
     
+    function clickfunction(field) {
+        field.addEventListener("click", playround)
+    }
 
+    function setToken() {
+        if (round % 2 === 1) {
+            playerToken = "X"}
+        else {
+            playerToken = "O"
+        }
+    }
+    
+    function playround(e) {
+        if (e.target.innerHTML === "") {
+        e.target.innerHTML = playerToken
+        round ++
+        setToken()
+        board[e.target.dataset.index] = playerToken
+        console.log(round)
+        console.log(board)
+        }
+    }
 
-}
+});
 
-function myfunction(){
-    document.getElementById("box1").innerText = "X"
-}
-
-
-document.getElementById("box1").addEventListener("click", myfunction)
-
-
-
-
-__REACT_DEVTOOLS_GLOBAL_HOOK__ = true;
+gameController()
+gameboard()
